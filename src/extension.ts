613775e7ed4dcf7592c3e38b9009d60d9d9e1cae
Fraser Hobbs <72457419+Fraser-Hobbs@users.noreply.github.com>
2022-03-13 21:25:17 +0000

    Light Refactoring

--- src/extension.ts
+++ src/extension.ts
@@ -1,36 +1,23 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { IconBrowserPanel } from './IconBrowserPanel';
-import { SidebarProvider } from './SidebarProvider';
+import { IconWebview } from './components/IconWebview';
+import { Sidebar } from './providers/Sidebar';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "hero-icon-browser" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-
 
 	context.subscriptions.push(vscode.commands.registerCommand('hero-icon-browser.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
+		IconWebview.createOrShow(context.extensionUri);
 		// Display a message box to the user
-		IconBrowserPanel.createOrShow(context.extensionUri);
-
 		vscode.window.showInformationMessage('Hello World from Hero Icon Browser!');
 	}));
 
-	const sidebarProvider = new SidebarProvider(context.extensionUri);
+	const sidebarProvider = new Sidebar(context.extensionUri);
 	context.subscriptions.push(vscode.window.registerWebviewViewProvider(
 		"hero-icon-browser-sidebar",
 		sidebarProvider
 	));
-
 
 }
 
